d3731a820b9fe815ed02673855c762ae370db99d
ianfeather <info@ianfeather.co.uk>
2014-05-06 23:53:02 +0100

    Add comments

--- .create-package.sh
+++ .create-package.sh
@@ -12,19 +12,28 @@ echo -e "${blue}Copy across global files${none}"
 cp -r global/ _package-to-deploy/
 
 echo ""
+echo -e "${blue}JSHint${none}"
+jshint blog/source/javascripts/ianf.js
+
+echo ""
+echo -e "${blue}Uglify our JS${none}"
+cat blog/source/javascripts/ianf.js blog/source/javascripts/prism.js > blog/source/javascripts/ianf.min.js
+uglifyjs blog/source/javascripts/ianf.min.js -o blog/source/javascripts/ianf.min.js
+
+echo ""
 echo -e "${blue}Compile the blog with jekyll${none}"
 cd blog
 jekyll build
 
 echo ""
 echo -e "${blue}Compile our stylesheet${none}"
-sass sass/screen.sass:stylesheets/screen.css
+sass -t compressed sass/screen.sass:stylesheets/screen.css
 cp -r stylesheets site/
 
 echo ""
 echo -e "${blue}Copy across the blog package${none}"
 cd ..
-cp -r blog/site/ ./_package-to-deploy/ 
+cp -r blog/site/ ./_package-to-deploy/
 
 echo ""
 echo -e "${blue}Compile presentation stylesheet${none}"
@@ -35,6 +44,11 @@ cd ../..
 echo ""
 echo -e "${blue}Copy across the presentations package${none}"
 cp -r presentations _package-to-deploy/
+
+echo ""
+echo -e "${blue}Clean up JS${none}"
+rm blog/source/javascripts/ianf.min.js
+
 
 # TODO
 # Configure scp access and write a deploy job
